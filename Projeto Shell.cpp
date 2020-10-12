37377c31033bacbac4fd4ceee4568f7b6c15bed9
Daniel Nunes <57506420+danixnn@users.noreply.github.com>
2020-10-12 16:13:55 -0300

    Update Projeto Shell.cpp

--- Projeto Shell.cpp
+++ Projeto Shell.cpp
@@ -2,14 +2,41 @@
 #include <string.h>
 #include<stdlib.h>
 
+void erroduploesp(){
+	printf("Comando invalido, verifique os espacos\n");
+}
+
+
 main(){
-char linha[512];
-do{
-	
-printf("meu shell> ");
-fflush(stdin);
-gets(linha);
+	int i=0,pos,n;
+	char linha[512];
+	do{
+		i=0;
+		printf("meu shell> ");
+		fflush(stdin);
+		gets(linha);
+		
+		n = strlen(linha); //ver tamanho da linha
+		
+		if(n != 0){			//se linha for diferente de vazia separar comandos	
+			
+			while(linha[i] != '\0'){
+			
+				if(linha[i] == ' '){
+					if(linha[i-1] == ' '){
+						erroduploesp();				//chamar função erro espaço duplo
+						break;
+					}
+				}
+				
+				//achar metodo para separar comandos
+				
+				i++;	
+			}
+		}
+		
+			
+	}while(strcmp(linha,"quit") != 0);
 
-}while(strcmp(linha,"quit") != 0);
-
+	return 0;
 }
